9e48bfe4eed6bad0b68549a49e0d321b47298646
natalia.puig <natalia.puig@basetis.com>
2023-06-06 13:19:19 +0200

    proof

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input type="text" v-model="value1" />
+    <input type="text" v-model="value2" />
+    <button @click="clickButton()">CLICK</button>
+    <div>SUM:{{ sum }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data() {
+    return { value1: null, value2: null, sum: null };
+  },
+  methods: {
+    clickButton() {
+      this.sum = Number(this.value1) + Number(this.value2);
+    },
+  },
+};
+</script>
