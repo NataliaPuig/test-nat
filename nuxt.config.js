e4db841a547f542403282526cc54c8eb3e23f0b4
natalia.puig <natalia.puig@basetis.com>
2023-06-07 09:39:03 +0200

    install

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,39 @@
-export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  ssr: true,
-  head: {
-    title: "test-nat",
-    htmlAttrs: {
-      lang: "en",
+import { truncate } from "fs/promises";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "We bcn test",
+      meta: [
+        { name: "description", content: "All the best products in one place" },
+        {
+          name: "google-site-verification",
+          content: "ugIsV3zEV6OO2pd3FPdhCbz0QkjxsaIuLMKimFWpQ6I",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
     },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+    baseURL: "/we-bcn-test/",
+  },
+  ssr: true,
+  modules: ["nuxt-simple-sitemap"],
+  sitemap: {
+    hostname: "https://NataliaPuig.github.io/test-nat",
+    gzip: true,
+    routes: [
+      "/",
+      "/about",
+      "/filter",
+      "/details/dotnetdot/",
+      "/details/frontend_cluster/",
+      "/details/php_lovers/",
+      "/details/pythonises/",
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["element-ui/lib/theme-chalk/index.css"],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["@/plugins/element-ui"],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  router: {
-    base: "https://NataliaPuig.github.io/test-nat/",
-  },
-  build: {
-    transpile: [/^element-ui/],
-  },
-};
+});
