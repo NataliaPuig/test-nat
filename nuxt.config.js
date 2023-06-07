e267e4db11f3c363509f0dd9fc0b1deccc8d1fe8
natalia.puig <natalia.puig@basetis.com>
2023-06-07 10:36:11 +0200

    truncate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,23 @@
-import { truncate } from "fs/promises";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default {
   router: {
-    base: "https://NataliaPuig.github.io/test-nat/",
+    base: "/test-nat/",
   },
-  app: {
-    head: {
-      title: "We bcn test",
-      meta: [
-        { name: "description", content: "All the best products in one place" },
-        {
-          name: "google-site-verification",
-          content: "ugIsV3zEV6OO2pd3FPdhCbz0QkjxsaIuLMKimFWpQ6I",
-        },
-      ],
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
-        },
-      ],
-    },
-    baseURL: "/test-nat/",
+  head: {
+    title: "We bcn test",
+    meta: [
+      { name: "description", content: "All the best products in one place" },
+      {
+        name: "google-site-verification",
+        content: "ugIsV3zEV6OO2pd3FPdhCbz0QkjxsaIuLMKimFWpQ6I",
+      },
+    ],
+    link: [
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+      },
+    ],
   },
   ssr: true,
   modules: ["nuxt-simple-sitemap"],
@@ -39,4 +34,4 @@ export default defineNuxtConfig({
       "/details/pythonises/",
     ],
   },
-});
+};
